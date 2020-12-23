50b312f07351ec66709e93f5ffb4ccbfc6159091
Azeem Bande-Ali <me@azeemba.com>
2020-12-22 22:01:03 -0500

    Assign velocity properly

--- ChoreographyHive/choreography/choreos/bird_flap.py
+++ ChoreographyHive/choreography/choreos/bird_flap.py
@@ -1,5 +1,8 @@
 import csv
+from math import tan
+from typing import Dict, List
 from collections import defaultdict
+from rlbot.agents.base_agent import SimpleControllerState
 from rlbot.utils.structures.game_interface import GameInterface
 from rlbot.utils.game_state_util import (
     GameState,
@@ -13,7 +16,22 @@ from rlbot.utils.game_state_util import (
 from choreography.choreography import Choreography
 from choreography.common.preparation import HideBall, LetAllCarsSpawn
 
-from choreography.group_step import DroneListStep, StepResult, PerDroneStep
+from choreography.group_step import (
+    DroneListStep,
+    StepResult,
+    PerDroneStep,
+    BlindBehaviorStep,
+)
+
+START_ANGLE = 30.0 / 180.0
+ANGLE_SPEED = 20.0 / 180.0
+
+
+def list_to_indexed_map(items):
+    indexed = {}
+    for i in range(len(items)):
+        indexed[i] = items[i]
+    return indexed
 
 
 class BirdFlap(Choreography):
@@ -39,14 +57,70 @@ class BirdFlap(Choreography):
         self.sequence.clear()
 
         self.sequence.append(LetAllCarsSpawn(self.get_num_bots()))
+        self.sequence.append(BlindBehaviorStep(SimpleControllerState(), 2))
         self.sequence.append(DroneListStep(self.line_up))
+        self.sequence.append(BlindBehaviorStep(SimpleControllerState(), 2))
+        self.sequence.append(DroneListStep(self.flap))
+        self.sequence.append(BlindBehaviorStep(SimpleControllerState(), 2))
+        self.sequence.append(DroneListStep(self.score))
+
+    def make_locations(self, angle, length) -> List[Vector3]:
+        last = Vector3(0, 0, 500)
+        positions = []
+        y_spacing = 100
+        x_spacing = -30
+        last_angle = angle
+        for _ in range(length):
+            cur_angle = last_angle + angle
+            location = Vector3(
+                last.x + x_spacing,
+                last.y + y_spacing,
+                last.z + tan(cur_angle) * y_spacing,
+            )
+            positions.append(location)
+            last = location
+            last_angle = cur_angle
+        return positions
+
+    def calc_velo(self, now: Vector3, future: Vector3, dt):
+        velo = Vector3(
+            (future.x - now.x) / dt, (future.y - now.y) / dt, (future.z - now.z) / dt
+        )
+        return velo
+
+    def calc_angle(self, elapsed):
+        angle = START_ANGLE
+        if elapsed < 2:
+            angle -= elapsed * ANGLE_SPEED
+        else:
+            angle += -2 * ANGLE_SPEED + (elapsed - 2) * ANGLE_SPEED
+        return angle
+
+    def make_car_state(self, elapsed, length):
+        car_stats = []
+        dt = 1.0 / 60.0
+        location_now = self.make_locations(self.calc_angle(elapsed), length)
+        location_future = self.make_locations(self.calc_angle(elapsed + dt), length)
+        for (now, future) in zip(location_now, location_future):
+            velo = self.calc_velo(now, future, dt)
+            car_stats.append(CarState(Physics(location=now, velocity=velo)))
+
+        return list_to_indexed_map(car_stats)
 
     def line_up(self, packet, drones, start_time) -> StepResult:
-        offset_z = 500
-        car_states = {}
-        for drone in drones:
-            row = self.data[drone.index][0]
-            location = Vector3(row["x"]*2, row["y"]*2, row["z"]*2 + offset_z)
-            car_states[drone.index] = CarState(Physics(location=location))
+        car_states = self.make_car_state(0, self.get_num_bots())
         self.game_interface.set_game_state(GameState(cars=car_states))
+        return StepResult(finished=True)
+
+    def flap(self, packet, drones, start_time) -> StepResult:
+        elapsed = packet.game_info.seconds_elapsed - start_time
+        car_states = self.make_car_state(elapsed, self.get_num_bots())
+        self.game_interface.set_game_state(GameState(cars=car_states))
+
+        done = elapsed > 4
+        return StepResult(finished=done)
+
+    def score(self, packet, drones, start_time) -> StepResult:
+        ball = BallState(physics=Physics(location=Vector3(0, 5120 + 800, 200)))
+        self.game_interface.set_game_state(GameState(ball=ball))
         return StepResult(finished=True)
